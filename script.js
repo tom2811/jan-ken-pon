d343b5b8a9c8b51de463c7ae5062a07f70882095
tom2811 <oaksoe.thein01@gmail.com>
2024-01-04 18:55:24 +0630

    Added Javascript.

---
+++ script.js
@@ -0,0 +1,62 @@
+const choices = ["rock", "paper", "scissors"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+let playerScore = 0;
+let computerScore = 0;
+
+
+function playGame(playerChoice) {
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+    let result = "";
+    console.log(computerChoice);
+
+    if (playerChoice === computerChoice) {
+        result = "It's a tie!";
+    }
+    else {
+        switch(playerChoice) {
+            case "rock":
+                result = (computerChoice === "scissors") ? "You Win!" : "You Lose!";
+                break;
+            case "paper":
+                result = (computerChoice === "rock") ? "You Win!" : "You Lose!";
+                break;
+            case "scissors":
+                result = (computerChoice === "paper") ? "You Win!" : "You Lose!";
+                break;
+        }
+    }
+
+    playerDisplay.textContent = `Player: ${playerChoice}`;
+    computerDisplay.textContent = `Computer: ${computerChoice}`;
+    resultDisplay.textContent = result;
+
+    resultDisplay.classList.remove("green", "red", "grey");
+
+    switch(result) {
+        case "You Win!":
+            resultDisplay.classList.add("green");
+            playerScore++;
+            playerScoreDisplay.textContent = playerScore;
+            break;
+        case "You Lose!":
+            resultDisplay.classList.add("red");
+            computerScore++;
+            computerScoreDisplay.textContent = computerScore;
+            break;
+        case "It's a tie!":
+            resultDisplay.classList.add("grey");
+    }
+}
+
+function reset() {
+    playerScore = 0;
+    playerScoreDisplay.textContent = playerScore;
+    computerScore = 0;
+    computerScoreDisplay.textContent = computerScore;
+    result = "";
+    resultDisplay.textContent = result;
+}
